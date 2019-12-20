f064e28d98fa27be31eba099a161d4291d25d2b7
Enrique Valenzuela <emvp84@gmail.com>
2019-12-19 21:59:14 -0500

    one more trial

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,24 +1,28 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    slackSend   color: "good", 
-                message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful",
-                channel: "dev-null"
-  }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend   color: "danger", 
-                message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed",
-                channel: "dev-null"
-  }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend   color: "warning", 
-                message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable",
-                channel: "dev-null"
-  } 
-  else {
-    slackSend   color: "danger", 
-                message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear",
-                channel: "dev-null"
-  }
+def call(Map stageParams) {
+    buildResult = stageParams.buildResult
+    success_channel = stageParams.success_channel
+    fail_channel = stageParams.fail_channel
+
+    if ( buildResult == "SUCCESS" ) {
+        slackSend   color: "good", 
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful",
+                    channel: success_channel
+    }
+    else if( buildResult == "FAILURE" ) { 
+        slackSend   color: "danger", 
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed",
+                    channel: fail_channel
+    }
+    else if( buildResult == "UNSTABLE" ) { 
+        slackSend   color: "warning", 
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable",
+                    channel: fail_channel
+    } 
+    else {
+        slackSend   color: "danger", 
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear",
+                    channel: fail_channel
+    }
 }

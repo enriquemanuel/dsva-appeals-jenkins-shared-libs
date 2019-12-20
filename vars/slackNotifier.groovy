480d71313507bbb1ee73e59c73fa34dd0607fb98
Enrique Valenzuela <emvp84@gmail.com>
2019-12-19 22:03:22 -0500

    modified once more

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -8,21 +8,21 @@ def call(Map stageParams) {
     if ( buildResult == "SUCCESS" ) {
         slackSend   color: "good", 
                     message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful",
-                    channel: success_channel
+                    
     }
     else if( buildResult == "FAILURE" ) { 
         slackSend   color: "danger", 
                     message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed",
-                    channel: fail_channel
+                    
     }
     else if( buildResult == "UNSTABLE" ) { 
         slackSend   color: "warning", 
                     message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable",
-                    channel: fail_channel
+                    
     } 
     else {
         slackSend   color: "danger", 
                     message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear",
-                    channel: fail_channel
+                    
     }
 }

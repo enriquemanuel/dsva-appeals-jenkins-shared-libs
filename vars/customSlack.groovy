a6a76407a8250e21540e76d211c2ec2e4c478176
Enrique Valenzuela <emvp84@gmail.com>
2019-12-19 22:26:23 -0500

    method to madness

--- vars/customSlack.groovy
+++ vars/customSlack.groovy
@@ -11,15 +11,18 @@ def call(Map stageParams) {
         }
         else if( buildResult == "FAILURE" ) { 
             slackSend   color: "danger", 
-                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed",
+                        channel: stageParams.channel
         }
         else if( buildResult == "UNSTABLE" ) { 
             slackSend   color: "warning", 
-                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable",
+                        channel: stageParams.channel
         } 
         else {
             slackSend   color: "danger", 
-                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"
+                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear",
+                        channel: stageParams.channel
                         
         }
     }

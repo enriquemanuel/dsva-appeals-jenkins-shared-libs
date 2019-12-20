08c26d9d76eb5ad7afa9d2ca53be0dd4018ff2c5
Enrique Valenzuela <emvp84@gmail.com>
2019-12-20 15:40:56 -0500

    corrected

--- vars/customSlack.groovy
+++ vars/customSlack.groovy
@@ -13,17 +13,17 @@ def call(Map stageParams) {
         }
         else if( buildResult == "FAILURE" ) {
             slackSend   color: "danger",
-                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed",
+                        message: message,
                         channel: stageParams.channel
         }
         else if( buildResult == "UNSTABLE" ) {
             slackSend   color: "warning",
-                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable",
+                        message: message,
                         channel: stageParams.channel
         }
         else {
             slackSend   color: "danger",
-                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear",
+                        message: message,
                         channel: stageParams.channel
 
         }

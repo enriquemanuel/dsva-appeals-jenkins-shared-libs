19b67e23ed6a85ae8ea9c0f56b23f965186d963d
Enrique Valenzuela <emvp84@gmail.com>
2019-12-19 22:09:00 -0500

    removing comma

--- vars/customSlack.groovy
+++ vars/customSlack.groovy
@@ -4,23 +4,18 @@ def call(Map stageParams) {
     buildResult = stageParams.buildResult
     if ( buildResult == "SUCCESS" ) {
         slackSend   color: "good", 
-                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful",
-
-                    
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
     }
     else if( buildResult == "FAILURE" ) { 
         slackSend   color: "danger", 
-                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed",
-                    
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
     }
     else if( buildResult == "UNSTABLE" ) { 
         slackSend   color: "warning", 
-                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable",
-                    
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
     } 
     else {
         slackSend   color: "danger", 
-                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear",
-                    
+                    message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"
     }
 }

cbcb9917ce3f95ac6428e6308b74094fa419a45b
Enrique Valenzuela <emvp84@gmail.com>
2019-12-20 15:36:31 -0500

    new method

--- vars/customSlack.groovy
+++ vars/customSlack.groovy
@@ -2,28 +2,30 @@
 
 def call(Map stageParams) {
     buildResult = stageParams.buildResult
+    message = stageParams.message ? stageParams.message : "${buildResult} | Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER}"
+
     try{
         if ( buildResult == "SUCCESS" ) {
-            slackSend   color: "good", 
-                        message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful",
+            slackSend   color: "good",
+                        message: message
                         channel: stageParams.channel
-                        
+
         }
-        else if( buildResult == "FAILURE" ) { 
-            slackSend   color: "danger", 
+        else if( buildResult == "FAILURE" ) {
+            slackSend   color: "danger",
                         message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed",
                         channel: stageParams.channel
         }
-        else if( buildResult == "UNSTABLE" ) { 
-            slackSend   color: "warning", 
+        else if( buildResult == "UNSTABLE" ) {
+            slackSend   color: "warning",
                         message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable",
                         channel: stageParams.channel
-        } 
+        }
         else {
-            slackSend   color: "danger", 
+            slackSend   color: "danger",
                         message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear",
                         channel: stageParams.channel
-                        
+
         }
     }
     catch(err) {
